e2b708814b4a32da95392ffbb2bdc7c16bfed8ef
duncan4123 <duncan4123@users.noreply.github.com>
2026-06-30 06:33:20 +1000

    fix: align DoltLite init with external pack release

--- examples/beads-doltlite/embed.go
+++
@@ -1,9 +0,0 @@
-// Package beadsdoltlite embeds the built-in DoltLite beads provider pack.
-package beadsdoltlite
-
-import "embed"
-
-// PackFS contains the DoltLite beads provider pack files.
-//
-//go:embed pack.toml doctor commands examples formulas orders skills all:assets
-var PackFS embed.FS
